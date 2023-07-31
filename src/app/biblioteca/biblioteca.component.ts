ff734f329e1eb70ac3a0b3a595eaabf38ab27f96
gere112 <gerezellotti2018@gmail.com>
2023-07-31 14:42:22 -0300

    Estructura_de_archivos

---
+++ src/app/biblioteca/biblioteca.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-biblioteca',
+  templateUrl: './biblioteca.component.html',
+  styleUrls: ['./biblioteca.component.css'],
+})
+export class BibliotecaComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
